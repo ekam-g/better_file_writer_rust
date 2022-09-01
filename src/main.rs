21742016e703d588e6ec16806fd883e9ba5c22f2
Carghai74 <94583299+carghai@users.noreply.github.com>
2022-08-31 22:46:52 -0400

    final

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,47 @@
-use make_file::make_dic;
+//! Make files easier
 
-pub mod files;
-mod make_file;
+mod files;
 fn main() {
-    make_dic("test/yes/cool/yes/farf".to_string()).expect("");
+    make_folders("test/yes/cool/yes/farf".to_string()).expect("");
+}
+
+/// Loops and creates directories where you want them
+///
+/// this will create multiple directories even if some/path does not exist.
+/// # Examples
+///
+/// ```
+/// make_dic("some/path/test".to_string()).expect("fail");
+///
+///```
+///
+use std::{fs, io::Error};
+
+pub fn make_folders(path: String) -> Result<(), Error> {
+    let directory_error = fs::create_dir(&path);
+    return match directory_error {
+        Ok(_) => Ok(()),
+        Err(mut error) => {
+            let mut success = false;
+            let where_file = path.split("/");
+            let mut location: String = "".to_string();
+            for i in where_file {
+                location = location + i + "/";
+                let directory_error = fs::create_dir(&location);
+                match directory_error {
+                    Ok(_) => {
+                        success = true;
+                    }
+                    Err(e) => {
+                        error = e;
+                    }
+                };
+            }
+            if success {
+                Ok(())
+            } else {
+                Err(error)
+            }
+        }
+    };
 }
